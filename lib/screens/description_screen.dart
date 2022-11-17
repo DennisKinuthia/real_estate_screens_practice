ca095cfa51656d04d30bedf190ea1895ba765b21
futureNow_team254 <dennis@futurenow.link>
2022-11-17 16:34:51 +0400

    (fix bug)remove the safe area on the description page for uniformity

--- lib/screens/description_screen.dart
+++ lib/screens/description_screen.dart
@@ -18,169 +18,167 @@ class Description extends StatelessWidget {
     final ThemeData themeData = Theme.of(context);
     const double padding = 20.0;
     const EdgeInsets sidePadding = EdgeInsets.symmetric(horizontal: padding);
-    return SafeArea(
-      child: Scaffold(
-        body: SizedBox(
-          width: size.width,
-          height: size.height,
-          child: Stack(
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Stack(
-                    children: <Widget>[
-                      Image(
-                        image: AssetImage(itemData['image']),
-                      ),
-                      Positioned(
-                        top: 20.0,
-                        width: size.width,
-                        child: Padding(
-                          padding: sidePadding,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.pop(context);
-                                },
-                                child: BorderBox(
-                                  width: 50.0,
-                                  height: 50.0,
-                                  child: Icon(
-                                    Icons.arrow_back,
-                                    color: colorBlack,
-                                  ),
-                                ),
-                              ),
-                              BorderBox(
+    return Scaffold(
+      body: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Stack(
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage(itemData['image']),
+                    ),
+                    Positioned(
+                      top: 50.0,
+                      width: size.width,
+                      child: Padding(
+                        padding: sidePadding,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: BorderBox(
                                 width: 50.0,
                                 height: 50.0,
                                 child: Icon(
-                                  Icons.favorite_border,
+                                  Icons.arrow_back,
                                   color: colorBlack,
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  addVerticalSpace(padding),
-                  Padding(
-                    padding: sidePadding,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              formatCurrency(itemData['amount']),
-                              style: themeData.textTheme.headline1,
                             ),
-                            addVerticalSpace(3.0),
-                            Text(
-                              itemData['address'],
-                              style: themeData.textTheme.bodyText2,
+                            BorderBox(
+                              width: 50.0,
+                              height: 50.0,
+                              child: Icon(
+                                Icons.favorite_border,
+                                color: colorBlack,
+                              ),
                             ),
                           ],
                         ),
-                        BorderBox(
-                          width: 110.0,
-                          height: 50,
-                          child: Text(
-                            '20 Hours ago',
-                            style: themeData.textTheme.headline6,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  addVerticalSpace(padding),
-                  Padding(
-                    padding: sidePadding,
-                    child: Text(
-                      'House Information',
-                      style: themeData.textTheme.headline4,
-                    ),
-                  ),
-                  addVerticalSpace(padding),
-                  SingleChildScrollView(
-                    physics: BouncingScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        {'Square Foot': itemData['area']},
-                        {'Bedrooms': itemData['bedrooms']},
-                        {'Bathrooms': itemData['bathrooms']},
-                        {'Garage': itemData['garage']}
-                      ]
-                          .map(
-                            (data) => Container(
-                              margin: const EdgeInsets.only(left: 30.0),
-                              child: Column(
-                                children: <Widget>[
-                                  BorderBox(
-                                    width: 65.0,
-                                    height: 65.0,
-                                    child: Text(
-                                      '${data.values.first}',
-                                      style: themeData.textTheme.headline4,
-                                    ),
-                                  ),
-                                  addVerticalSpace(10.0),
-                                  Text(
-                                    data.keys.first,
-                                    style: themeData.textTheme.headline6,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          )
-                          .toList(),
-                    ),
-                  ),
-                  addVerticalSpace(padding),
-                  Expanded(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      physics: BouncingScrollPhysics(),
-                      child: Padding(
-                        padding: sidePadding,
-                        child: Text(itemData['description']),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Positioned(
-                bottom: 30.0,
-                width: size.width,
-                child: Padding(
+                  ],
+                ),
+                addVerticalSpace(padding),
+                Padding(
                   padding: sidePadding,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      OptionButton(
-                        text: 'Text',
-                        icon: Icons.message,
-                        width: size.width * 0.30,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            formatCurrency(itemData['amount']),
+                            style: themeData.textTheme.headline1,
+                          ),
+                          addVerticalSpace(3.0),
+                          Text(
+                            itemData['address'],
+                            style: themeData.textTheme.bodyText2,
+                          ),
+                        ],
                       ),
-                      OptionButton(
-                        text: 'Call',
-                        icon: Icons.call,
-                        width: size.width * 0.30,
+                      BorderBox(
+                        width: 110.0,
+                        height: 50,
+                        child: Text(
+                          '20 Hours ago',
+                          style: themeData.textTheme.headline6,
+                        ),
                       ),
                     ],
                   ),
                 ),
+                addVerticalSpace(padding),
+                Padding(
+                  padding: sidePadding,
+                  child: Text(
+                    'House Information',
+                    style: themeData.textTheme.headline4,
+                  ),
+                ),
+                addVerticalSpace(padding),
+                SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      {'Square Foot': itemData['area']},
+                      {'Bedrooms': itemData['bedrooms']},
+                      {'Bathrooms': itemData['bathrooms']},
+                      {'Garage': itemData['garage']}
+                    ]
+                        .map(
+                          (data) => Container(
+                            margin: const EdgeInsets.only(left: 30.0),
+                            child: Column(
+                              children: <Widget>[
+                                BorderBox(
+                                  width: 65.0,
+                                  height: 65.0,
+                                  child: Text(
+                                    '${data.values.first}',
+                                    style: themeData.textTheme.headline4,
+                                  ),
+                                ),
+                                addVerticalSpace(10.0),
+                                Text(
+                                  data.keys.first,
+                                  style: themeData.textTheme.headline6,
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                addVerticalSpace(padding),
+                Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    physics: BouncingScrollPhysics(),
+                    child: Padding(
+                      padding: sidePadding,
+                      child: Text(itemData['description']),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 30.0,
+              width: size.width,
+              child: Padding(
+                padding: sidePadding,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    OptionButton(
+                      text: 'Text',
+                      icon: Icons.message,
+                      width: size.width * 0.30,
+                    ),
+                    OptionButton(
+                      text: 'Call',
+                      icon: Icons.call,
+                      width: size.width * 0.30,
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
